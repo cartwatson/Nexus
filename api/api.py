847748d43f25c6325eb0f20b44ea8af36b6888ff
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-03-26 21:12:38 -0700

    update: fix connection bug; reuse code for droid specific images; store requests in db;

--- api/api.py
+++ api/api.py
@@ -1,4 +1,6 @@
 import os
+import time
+from datetime import datetime
 from flask import Flask, jsonify
 import psycopg2
 
@@ -22,39 +24,53 @@ def add_tracked_satellite(satellite_id):
 def get_image_from_satellite(satellite_id):
     """request image of a tracked satellite, store these requests"""
 
-    cur.execute(
-        f"""SELECT img FROM images
-        WHERE id_sat='{satellite_id}'
-        """
-    )
+    request = f"SELECT img FROM images WHERE id_sat='{satellite_id}'"
+
+    # Make request for image
+    cur.execute(request)
     results = cur.fetchall()
 
-    if not results:
-        return jsonify({"ERROR": f"No images found for {satellite_id}"}), 204
+    success = len(results)
+    # Record request for image
+    cur.execute(
+        f"""INSERT INTO requests (id_sat, time, request, img_returned) VALUES
+        ('{satellite_id}', '{datetime.now()}', \
+        '{request.replace("'", "''")}', '{str(success).upper()}')
+        """
+    )
 
-    return jsonify({results})
+    if not success:
+        # NOTE: possibility this should be a 204
+        return jsonify({"ERROR": f"{satellite_id} exists but no images found"}), 200
+    # TODO: do something with results as they'll be raw image data
+    return jsonify({"results": results}), 200
 
 
 @app.route("/get-droid-images")
 def get_images_from_droid():
     """get images of tracked satellite take by DROID"""
-    cur.execute("SELECT img FROM images WHERE id_sat='DROID'")
-    results = cur.fetchall()
-    if not results:
-        return jsonify({"ERROR": "No images found for DROID"}), 204
 
-    return jsonify({results})
+    # NOTE: if get_image_from_satellite later takes image_id, return all images
+    return get_image_from_satellite('DROID')
 
 
 if __name__ == "__main__":
-    # init psycopg2
-    conn = psycopg2.connect(
-        host="pg",
-        dbname="takehome",
-        user="takehome",
-        password="takehome",
-        port="5432",
-    )
+    connected = False
+    while not connected:
+        try:
+            # init psycopg2
+            conn = psycopg2.connect(
+                host="pg",
+                dbname="takehome",
+                user="takehome",
+                password="takehome",
+                port="5432",
+            )
+            print("connection established")
+            connected = True
+        except:  # noqa: E722
+            print("connection failed: attempting reconnect in 1sec")
+            time.sleep(1)
 
     cur = conn.cursor()
     conn.autocommit = True
