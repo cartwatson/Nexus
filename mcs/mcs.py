9e95d60b55f191ee7be4b8b17203e70916127512
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-03-26 23:14:03 -0700

    init mcs and satellite

--- mcs/mcs.py
+++ mcs/mcs.py
@@ -1,10 +1,55 @@
 import time
+import random
+import psycopg2
+
+# NOTES:
+# upload images to psql
+# https://www.postgresql.org/docs/7.4/jdbc-binary-data.html
+
+
+# When the MCS is able to communicate with the satellite, send imaging requests to the satellite that the MCS finds in the database.
+def send_image_requests():
+    """get all images from satellite"""
+    # send requests to satellite for unfulfilled requests
+    pass
+
+
+# Periodically send a “download all images” command to the satellite to get all images stored on the satellite and insert into the database.
+def download_all_images():
+    """download all images from satellite to db"""
+    # query for images
+    # push image to db
+    pass
 
 
 def main():
+    connected = False
+    while not connected:
+        try:
+            # init psycopg2
+            conn = psycopg2.connect(
+                host="pg",
+                dbname="takehome",
+                user="takehome",
+                password="takehome",
+                port="5432",
+            )
+            print("connection established")
+            connected = True
+        except:  # noqa: E722
+            print("connection failed: attempting reconnect in 1sec")
+            time.sleep(1)
+
+    cur = conn.cursor()
+    conn.autocommit = True
+
     while True:
-        print("talking to sat...")
-        time.sleep(5)
+        print("ESTABLISHING COMMUNICATION WITH DROID")
+        send_image_requests()
+        download_all_images()
+        print("COMMUNICATION LOST...")
+        # system to intermittetly create contacts with satellite
+        time.sleep(random.randint(5, 10))
 
 
 if __name__ == "__main__":
--- satellite/satellite.py
+++ satellite/satellite.py
@@ -1,11 +1,22 @@
-import time
+import os
+from datetime import datetime
+from flask import Flask, jsonify
+
+app = Flask(__name__)
 
 
-def main():
-    while True:
-        print("establishing contact...")
-        time.sleep(5)
+@app.route("/take_image/<target_sat>")
+def take_image(target_sat):
+    """take an image of a target satellite"""
+    pass
+
+
+@app.route("/transmit_image/<target_sat>")
+def transmit_image(target_sat):
+    """transmit image of target sat to mcs """
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
