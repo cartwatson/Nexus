a0e474924436d2c253e9f323c4bbb0630460bac6
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-03-27 00:17:08 -0700

    feat: add db functionality

--- mcs/mcs.py
+++ mcs/mcs.py
@@ -3,26 +3,69 @@ import random
 import psycopg2
 
 # NOTES:
+# request looks like
+# datetime                                       target_sat, fulfillled, pending
+# (datetime.datetime(2024, 3, 27, 7, 1, 44, 308839), 'object0', False, False)
 # upload images to psql
 # https://www.postgresql.org/docs/7.4/jdbc-binary-data.html
 
 
 # When the MCS is able to communicate with the satellite, send imaging requests to the satellite that the MCS finds in the database.
-def send_image_requests():
-    """get all images from satellite"""
-    # send requests to satellite for unfulfilled requests
-    pass
+def send_image_requests(cur):
+    """send requests to satellite for unfulfilled requests"""
+
+    # get image requests # sort out ones that are already fulfilled
+    cur.execute("SELECT * FROM requests WHERE fulfilled='FALSE' AND pending='FALSE'")
+    results = cur.fetchall()
+
+    pending_requests = len(results)
+
+    if not pending_requests:
+        return
+
+    print("REQUESTING...") # DEBUG: REMOVE BEFORE PROD
+    for request in results:
+        print(request) # DEBUG: REMOVE BEFORE PROD
+        # send request to droid to take image of target satellite
+        # mark request as pending
+        cur.execute(f"UPDATE requests SET pending='TRUE' WHERE time='{request[0]}'")
+    return
 
 
 # Periodically send a “download all images” command to the satellite to get all images stored on the satellite and insert into the database.
-def download_all_images():
+def download_all_images(cur):
     """download all images from satellite to db"""
-    # query for images
-    # push image to db
+
+    # set image request to fulfilled in db table
+    cur.execute("SELECT * FROM requests WHERE fulfilled='FALSE' AND pending='TRUE'")
+    results = cur.fetchall()
+
+    unfulfilled_requests = len(results)
+
+    if not unfulfilled_requests:
+        return
+
+    print("DOWNLOADING...") # DEBUG: REMOVE BEFORE PROD
+    for request in results:
+        print(request) # DEBUG: REMOVE BEFORE PROD
+        # query droid for image based on request
+        # if image exists, update request
+        cur.execute(f"UPDATE requests SET fulfilled='TRUE' WHERE time='{request[0]}'")
+        # push image to db
     pass
 
 
-def main():
+def main(cur):
+    while True:
+        print("ESTABLISHING COMMUNICATION WITH DROID")
+        download_all_images(cur)
+        send_image_requests(cur)
+        print("COMMUNICATION LOST...")
+        # system to intermittetly create contacts with satellite
+        time.sleep(random.randint(5, 10))
+
+
+if __name__ == "__main__":
     connected = False
     while not connected:
         try:
@@ -37,20 +80,10 @@ def main():
             print("connection established")
             connected = True
         except:  # noqa: E722
-            print("connection failed: attempting reconnect in 1sec")
+            print("connection to db failed: attempting reconnect in 1sec")
             time.sleep(1)
 
     cur = conn.cursor()
     conn.autocommit = True
 
-    while True:
-        print("ESTABLISHING COMMUNICATION WITH DROID")
-        send_image_requests()
-        download_all_images()
-        print("COMMUNICATION LOST...")
-        # system to intermittetly create contacts with satellite
-        time.sleep(random.randint(5, 10))
-
-
-if __name__ == "__main__":
-    main()
+    main(cur)
