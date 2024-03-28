88caf6fbae8b6cbbf30fcfe3ae60be81012d5532
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-03-28 00:21:13 -0700

    add satellite demo images; add ability to transmit images

--- satellite/satellite.py
+++ satellite/satellite.py
@@ -1,22 +1,32 @@
 import os
+import base64
+import random
 from datetime import datetime
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/take_image/<target_sat>")
+@app.route("/take-image/<target_sat>")
 def take_image(target_sat):
     """take an image of a target satellite"""
-    pass
+
+    # obtain image of "satellite"
+    with open(f"hidden-images/sat{random.randint(0, 3)}.png", "rb") as image:
+        f = image.read()
+        b = bytearray(f)
+        image_bytearray = base64.b64encode(b).decode('utf-8')
+
+    # return bytea
+    return jsonify({"image_data": image_bytearray}), 200
 
 
-@app.route("/transmit_image/<target_sat>")
+@app.route("/transmit-image/<target_sat>")
 def transmit_image(target_sat):
     """transmit image of target sat to mcs"""
     pass
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5050))
     app.run(debug=True, host="0.0.0.0", port=port)
