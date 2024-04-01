1a2f14fc2f1b9e047e6e37c57383fe46d8243f5c
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-03-31 17:21:04 -0700

    feat: exchange hardcoded urls for key-value params; refactor: simplified return request

--- api/api.py
+++ api/api.py
@@ -4,37 +4,56 @@ import time
 import json
 from PIL import Image
 from datetime import datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 
 app = Flask(__name__)
 
+def return_json(success, message, data=None, status_code=500):
+    """Create response for api"""
+    return {
+        "Success": success,
+        "Message": message,
+        "Data" : data
+    }, status_code
 
-@app.route("/add-satellite/<sat_id>")
-def add_tracked_satellite(sat_id):
+
+# expects id of satellite to track as param on request
+@app.route("/track-satellite")
+def add_tracked_satellite():
     """Add tracked satellite to database"""
-
+    
+    sat_id = request.args.get('id')
+    # if no params provided, return list of all tracked satellites
+    if sat_id is None:
+        try:
+            cur.execute("SELECT id FROM satellites")
+            satellites = cur.fetchall()  # NOTE: check type here
+            return return_json(True, "Provided data is all tracked satellites.", satellites, 200)
+        except:
+            return return_json(False, "Unable to get list of tracked satellites!", None, 500)
+    
     try:
         cur.execute(
             f"""INSERT INTO satellites (id) VALUES
             ('{sat_id}')
             """
         )
-    except psycopg2.errors.lookup("23505"): # UniqueViolation
-        return {"Success": False,
-                "Message": f"Already tracking {sat_id}!",
-                "Data": None
-            }, 500
+    except psycopg2.errors.UniqueViolation:
+        return return_json(False, f"Already tracking {sat_id}!", None, 500)
+    except psycopg2.errors:
+        return return_json(False, f"Error attempting to add satellite as tracked in database!", None, 500)
 
-    return {"Success": True,
-            "Message": f"Now tracking {sat_id}.",
-            "Data": None
-        }, 200
+    return return_json(True, f"Now tracking{sat_id}", None, 200)
 
 
-@app.route("/request-image-of-satellite/<target_sat>")
-def request_image_of_satellite(target_sat):
+@app.route("/request-image")
+def request_image_of_satellite():
     """request image of a tracked satellite"""
+
+    target_sat = request.args.get('id')
+    if target_sat is None
+        return return_json(False, "No id provided!", None, 400)
 
     try:
         cur.execute(
@@ -43,15 +62,9 @@ def request_image_of_satellite(target_sat):
             """
         )
     except psycopg2.errors:
-        return {"Success": False,
-                "Message": f"Erorr submitting request for image of {sat_id}.",
-                "Data": None
-            }, 500
+        return return_json(False, f"Error submitting request for image of {sat_id}.", None, 500)
 
-   return {"Success": True,
-            "Message": f"Request for image of {sat_id} submitted.",
-            "Data": None
-        }, 200
+    return return_json(True, f"Request for image of {sat_id} submitted.", None, 200)
 
 
 def get_image(sat_id, taken_by=None):
@@ -74,22 +87,27 @@ def get_image(sat_id, taken_by=None):
         image = Image.open(io.BytesIO(image_bytes))
         image.save(image_save_dest)
     else:
-        return {"Success": False,
-                "Message": f"No image found in database for {sat_id}!",
-                "Data": base64_encoded_image
-            }, 500
+        return return_json(False, f"No image found in database for {sat_id}!", None, 500)
 
-    return {"Success": True,
-            "Message": f"Image of {sat_id} retrieved, saved in {image_save_dest}",
-            "Data": base64_encoded_image
-        }, 200
+    return return_json(True, f"Image of {sat_id} retrieved, saved in {image_save_dest}", base64_encoded_image, 200)
 
 
-@app.route("/get-images-from-droid/<sat_id>")
-def get_images_from_droid(sat_id):
+@app.route("/images")
+def get_images_from_droid():
     """get images of a tracked satellite taken by DROID"""
-    return get_image(sat_id, taken_by="DROID")
-
+        
+    try:
+        sat_id = request.args.get('id')
+        try:
+            droid_id = request.args.get('droid')
+            return get_image(sat_id, droid_id)
+        except:
+            # if droid id not specified, its implied its DROID
+            return get_image(sat_id, taken_by="DROID")
+    except: # TODO: find correct error code for when .get doesn't work
+        # TODO: could return all images, make request for all satellite ids and push request through
+        return return_json(False, "id of satellite not provided!", None, 400)
+    
 
 if __name__ == "__main__":
     connected = False
@@ -111,7 +129,7 @@ if __name__ == "__main__":
 
     cur = conn.cursor()
     conn.autocommit = True
-
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
 
