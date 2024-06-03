1a1ea21280320b19fdd577f02e477d89c98fcb30
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-06-02 18:55:49 -0700

    feat: navbar working

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 
+// TODO: move into modules
+// layouts
 import HeaderComponent from './components/layouts/Header';
 import SidebarComponent from './components/layouts/Sidebar'; 
 import FooterComponent from './components/layouts/Footer';
 
+// dashboards
 import HomeDashboard from './components/layouts/dashboards/HomeDashboard';
 import SatelliteDashboard from './components/layouts/dashboards/SatelliteDashboard'; 
 import DroidsDashboard from './components/layouts/dashboards/DroidsDashboard'; 
@@ -14,21 +17,21 @@ import ImagesDashboard from './components/layouts/dashboards/ImagesDashboard';
 
 const App: React.FC = () => {
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-            <HeaderComponent />
-            <Layout>
-                <Router>
-                    <Switch>
-                        <Route path="/" component={HomeDashboard} />
-                        <Route path="/satellites" component={SatelliteDashboard} />
-                        <Route path="/droids" component={DroidsDashboard} />
-                        <Route path="/images" component={ImagesDashboard} />
-                    </Switch>
-                </Router>
-                <SidebarComponent />
+        <Router>
+            <Layout style={{ minHeight: '100vh' }}>
+                <HeaderComponent />
+                <Layout>
+                    {/*<SidebarComponent />*/}
+                        <Switch>
+                            <Route exact path="/" component={HomeDashboard} />
+                            <Route path="/satellites" component={SatelliteDashboard} />
+                            <Route path="/droids" component={DroidsDashboard} />
+                            <Route path="/images" component={ImagesDashboard} />
+                        </Switch>
+                </Layout>
+                {/* <FooterComponent /> */}
             </Layout>
-            <FooterComponent />
-        </Layout>
+        </Router>
     );
 };
 
--- dashboard/src/components/layouts/Header.tsx
+++ dashboard/src/components/layouts/Header.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
-import { FileImageOutlined, AimOutlined, CameraOutlined } from '@ant-design/icons';
-// import './Header.css';
+import { HomeOutlined, FileImageOutlined, AimOutlined, CameraOutlined } from '@ant-design/icons';
 
 const { Header } = Layout;
 
 const HeaderComponent: React.FC = () => {
+    const location = useLocation();
+    const [selectedKey, setSelectedKey] = useState<string>(() => {
+        // Get the initial selected key based on the current page URL
+        const path = location.pathname;
+        switch (path) {
+            case '/':
+                return '1';
+            case '/satellites':
+                return '2';
+            case '/droids':
+                return '3';
+            case '/images':
+                return '4';
+            default:
+                return '';
+        }
+    });
+
+    useEffect(() => {
+        const path = location.pathname;
+        switch (path) {
+            case '/':
+                setSelectedKey('1');
+                break;
+            case '/satellites':
+                setSelectedKey('2');
+                break;
+            case '/droids':
+                setSelectedKey('3');
+                break;
+            case '/images':
+                setSelectedKey('4');
+                break;
+            default:
+                setSelectedKey('');
+        }
+    }, [location.pathname]);
+
     return (
-        <Header>
+        <Header style={{padding: '0'}}>
             <div className="logo" />
-            <Menu theme="dark" mode="horizontal">
-                <Menu.Item key="1" icon={<AimOutlined />}>Satellites</Menu.Item>
-                <Menu.Item key="2" icon={<CameraOutlined />}>Droids</Menu.Item>
-                <Menu.Item key="3" icon={<FileImageOutlined />}>Images</Menu.Item>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[selectedKey]}>
+                <Menu.Item key="1" icon={<HomeOutlined />}>
+                    <Link to="/">Nexus</Link>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<AimOutlined />}>
+                    <Link to="/satellites">Satellites</Link>
+                </Menu.Item>
+                <Menu.Item key="3" icon={<CameraOutlined />}>
+                    <Link to="/droids">Droids</Link>
+                </Menu.Item>
+                <Menu.Item key="4" icon={<FileImageOutlined />}>
+                    <Link to="/images">Images</Link>
+                </Menu.Item>
             </Menu>
-            <ul>
-                <li><a href='/satellites'>Satellites</a></li>
-                <li><a href='/droids'>Droids</a></li>
-                <li><a href='/images'>Images</a></li>
-            </ul>
         </Header>
     );
 };
