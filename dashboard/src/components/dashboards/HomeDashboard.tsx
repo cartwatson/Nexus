ee0c66256550919980644aece37cbd06545dbbb4
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-06-03 13:48:59 -0700

    feat: add basic content to pages/differentiate pages

---
+++ dashboard/src/components/dashboards/HomeDashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const HomeDashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>What is Nexus?</h1>
+            <p>Nexus is a demo ground station control system.</p>
+            <h1>How to use this site?</h1>
+            <p>Use the navbar at the top of the screen to access functionality based on targeted information.</p>
+        </div>
+    );
+};
+
+export default HomeDashboard;
+
